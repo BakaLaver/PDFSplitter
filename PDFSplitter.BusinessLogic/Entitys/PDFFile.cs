12b0590b9dd2b529897e680b3659c5ae4dbafd4f
BakaLover <BakaLover@DESKTOP-7MDCL9C>
2024-02-17 18:26:06 +0300

    Добавьте файлы проекта.

---
+++ PDFSplitter.BusinessLogic/Entitys/PDFFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFSplitter.BusinessLogic.Entity
+{
+    public class PDFFile
+    {
+
+    }
+}
---
+++ PDFSplitter.BusinessLogic/Interfaces/IPDFService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFSplitter.BusinessLogic.Interfaces
+{
+    public interface IPDFService
+    {
+    }
+}
